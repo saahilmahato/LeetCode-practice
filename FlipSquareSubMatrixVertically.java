ac242d6252bd1dcdc19dd0d16c78c4c73ce55596
saahilmahato <mahatosaahil4@gmail.com>
2026-03-20 13:00:00 +0545

    feat: find square sub matrix vertically

---
+++ FlipSquareSubMatrixVertically.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        for (int i = 0; i < k/2; i++) {
+            int topRowIndex = x + i;
+            int bottomRowIndex = x + k - 1 - i;
+
+            for (int j = 0; j < k; j++) {
+                int currentCol = y + j;
+
+                int temp = grid[topRowIndex][currentCol];
+                grid[topRowIndex][currentCol] = grid[bottomRowIndex][currentCol];
+                grid[bottomRowIndex][currentCol] = temp;
+            }
+        }
+
+        return grid;
+    }
+}
